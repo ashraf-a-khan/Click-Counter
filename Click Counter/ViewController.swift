8f35421979be9fbd6291a04d46adc318d2e7f604
ashraf-a-khan <khan.ashraf2311@gmail.com>
2020-01-02 12:37:08 -0500

    Initial Commit

---
+++ Click Counter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Click Counter
+//
+//  Created by Ashraf Khan on 1/2/20.
+//  Copyright © 2020 Ashraf Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
